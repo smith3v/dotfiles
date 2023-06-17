b78533ac94faa1944e4885f31f22bd5848d3ba64
Slava Kuznetsov <kuznetsovvv@gmail.com>
2023-06-17 09:54:00 +0200

    Switch EDITOR to nvim

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,7 @@
-set -gx EDITOR vim
+set -gx EDITOR nvim
 
 string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
 
 fish_add_path -aP /var/lib/flatpak/exports/bin
+fish_add_path -aP /home/neuron/bin
